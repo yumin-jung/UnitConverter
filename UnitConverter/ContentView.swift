d8ecfd2eacd6618cb773e22e5dc1eb28dad388e9
yumin-jung <jungym887@naver.com>
2025-08-28 01:49:31 +0900

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by yumin on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by yumin on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
